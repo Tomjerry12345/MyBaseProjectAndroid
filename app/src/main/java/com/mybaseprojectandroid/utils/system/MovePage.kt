a4538309663889cabfdd655918d02cd1480a4483
Doraemon <Andrialfiandi46@gmail.com>
2022-05-31 04:49:54 +0800

    Initial commit

---
+++ app/src/main/java/com/mybaseprojectandroid/utils/system/MovePage.kt
@@ -0,0 +1,15 @@
+package com.mybaseprojectandroid.utils.system
+
+import android.content.Intent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+
+fun moveNavigationTo(view: View, destination: Int) {
+    view.findNavController().navigate(destination)
+}
+
+fun moveIntentTo(start: ComponentActivity, destination: AppCompatActivity) {
+    val intent = Intent(start, destination::class.java)
+    start.startActivity(intent)
+}
